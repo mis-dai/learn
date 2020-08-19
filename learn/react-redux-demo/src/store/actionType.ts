7504ab86a2027cce6e12fad1c81d83c55b478959
mis_dai <847421302@qq.com>
2020-08-19 16:47:57 +0800

    update

--- learn/react-redux-demo/src/store/actionType.ts
+++
@@ -1,2 +0,0 @@
-export const ITEM_ADD = "changeItem"
-export const ITEM_REDUCE = "reduceItem"
--- learn/react-redux-demo/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'  // 引入createStore方法
-import reducer from './reducer'
-
-
-const store = createStore(reducer)          // 创建数据存储仓库
-export default store
