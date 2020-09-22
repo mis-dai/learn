da8c19e9c5defc18e90f799c05f75c485acb6672
mis_dai <847421302@qq.com>
2020-09-22 13:59:53 +0800

    引用bootstrap的渲染

--- node-demo/http/server.js
+++ node-demo/http/server.js
@@ -1,6 +1,7 @@
 // http 的模块 创建编写服务器
-var https = require('http')
-var fs = require('fs')
+var https = require('http');
+var fs = require('fs');
+var tp1 = require('art-template')
 
 //创建实例
 var server = https.createServer()
@@ -23,9 +24,20 @@ server.on('request',(req,res) => {
       name: '荒野',
       age: 15
     },
+  ];
+  var comments = [
+    {
+      message: "今天天气不错"
+    },
+    {
+      message: "我觉得这个小说作者文采不错"
+    },
+    {
+      message:"大家可以互相认识"
+    }
   ]
   // 编码类型 否则乱码
-  res.setHeader('Content-Type','text/plain;charset=utf-8')
+  // res.setHeader('Content-Type','text/plain;charset=utf-8')
   switch(url) {
     case '/':
       res.end('hello mis_dai')
@@ -38,9 +50,8 @@ server.on('request',(req,res) => {
       res.end(JSON.stringify(product))
       break;
     case '/view':
-      fs.readFile('../view/view.html',(err, data) => {
+      fs.readFile('../views/view.html',(err, data) => {
         if(err) {
-            // res.setHeader('Content-Type','text/plain;charset=utf-8')
           res.end('读取失败')
           } else {
             res.setHeader('Content-Type','text/html;charset=utf-8')
@@ -48,13 +59,33 @@ server.on('request',(req,res) => {
         }
       })
       break;
+    case '/comments': 
+      fs.readFile('../views/comments.html',(err,data) =>{
+        if (err) {
+          res.end("无法访问")
+          }
+        res.writeHead(200,{'Content-Type':'text/html'})
+        // 模板渲染
+        res.end(tp1.render(data.toString(),{
+          comments:comments
+          })
+        )    
+      })
+      break;
+    case '/lib/bootstrap/dist/css/bootstrap.css':
+      console.log('测试',url)
+      fs.readFile('..'+ url,(err, data) => {
+        if(err) {
+          res.end('不存在')
+        }
+        res.end(data)
+      })
+      break;
     default :
-    res.end(JSON.stringify("未知"));
-
+    res.end("未知");
   }
+
   // write 可以使用多次 但最后一定要end结尾 否则客户端一直等待
-  // res.write('hello.mis_dai')
-  // res.write('这是第二句')
   // res.end() //内容必须是二进制 或 字符串
 })
 
