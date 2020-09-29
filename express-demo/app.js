5b4c6b0d1a5e5260d8c3bbd1322633f9930a1641
mis_dai <847421302@qq.com>
2020-09-29 15:39:58 +0800

    update

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,42 +1,62 @@
 var express = require('express')
-
-var app = express()  //1.相当于 var app = createServer() 创建实例
+var bodyParser = require('body-parser')
+  //1.相当于 var app = createServer() 创建实例
+    var app = express() 
     var comments = [
-      {
-        message: '今天天气不错'
-      },
-      {
-        message: '这个作者的文采很优秀'
-      },
-      {
-        message: '我很喜欢这个作者写的一系列的作品'
-      },
+        {
+            message: '今天天气不错'
+        },
+        {
+            message: '这个作者的文采很优秀'
+        },
+        {
+            message: '我很喜欢这个作者写的一系列的作品'
+        },
     ]
     // 公开指定目录,可以直接访问路径
-    app.use('/public/', express.static('public')) //等同于app.use('/public/', express.static('./public/'))
-    app.use('/node_modules/', express.static('node_modules'))
-    app.engine('html',require('express-art-template'))
+        app.use('/views',express.static('views'))
+        app.use('/public/', express.static('public')) //等同于app.use('/public/', express.static('./public/'))
+        app.use('/node_modules/', express.static('node_modules'))
+
+    // 模板引擎 第一个参数为访问文件类型
+        app.engine('html',require('express-art-template'))
+
+    // 配置body-parser,编译post请求传递的数据格式
+        app.use(bodyParser.urlencoded({ extended: false}))
+        app.use(bodyParser.json())
+
     // 路由访问资源
-    //（1）以get 方法请求 / 的时候
-    app.get('/',(req, res) => {
-      res.send(`<h2>html模板</h2>`)
-    })
-    app.get('/main',(req, res) => {
-      res.send("测试")
-    })
-    app.get('/views',(req, res) => {
-      res.render('view.html',{
-        title: '管理系统'
-      })
-    })//默认在views,是个约定 里找模板
-    
-    app.get('/comments',(req, res) => {
-      res.render('view.html', {
-        comments: comments
-      })
-    })
+        //（1）以get 方法请求 / 的时候
+        app.get('/',(req, res) => {
+          res.send(`<h2>html模板</h2>`)
+        })
+        app.get('/main',(req, res) => {
+          res.send("测试")
+        })
+        app.get('/view',(req, res) => {
+          res.render('view.html',{
+            title: '管理系统',
+            comments: comments,
+          })
+        })//默认在views,是个约定文件夹里找模板
+        app.get('/comments',(req, res) => {
+          res.render('comments.html')
+        })
+        //（2）以post方法请求 /view 的时候
+          app.post('/comments',(req, res) => {
+              var comment = req.body
+              comments.push(comment)
+              res.redirect('view')
+          })
+        // //表单为get方法提交数据时
+            // app.get('/add',(req, res) => {
+            //   var adds = req.query
+            //   comments.push(adds)
+            //   res.redirect('/view') //重定向
+            // })
+
 
     // 2.监听接口
-    app.listen(3000,() => {
-      console.log("连接成功")
-    })
+        app.listen(3000,() => {
+          console.log("server is running")
+        })
--- express-demo/readme.md
+++ express-demo/readme.md
@@ -1,11 +1,14 @@
-1、文件加载可以省略 './'
+1.文件加载可以省略 './'
   fs.readFile('app.js')
 
   非核心模块或者第三方插件，模块加载文件不可以省略 './'
   如 require('./)
 
-2、使用第三方命令行工具nodemon实现修改完代码后 自动重启服务器 
+2.使用第三方命令行工具nodemon实现修改完代码后 自动重启服务器 
   node xxx.js 改为 nodemon xxx.js
 
-3、若要修改模板约定的指定views 目录，则可以使用
-  xx.set('views',要修改的目录路径)
+3.若要修改模板约定的指定views 目录，则可以使用
+  xx/实列.set('views',要修改的目录路径)
+
+4. res.query只能读取get 方法的请求传递的数据。在express中没有内置获取表单 post 请求体的API，借助第三方插件 body-parser
+//加入这个配置后 在req请求里多一个 body 的参数 res.body
