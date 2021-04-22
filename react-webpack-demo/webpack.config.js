88bda31ce62ae21f97ac27dcac8a24bfc957b246
mis-dai <67568929+mis-dai@users.noreply.github.com>
2021-04-22 17:27:33 +0800

    Update webpack.config.js
    修改打包基本配置

--- react-webpack-demo/webpack.config.js
+++ react-webpack-demo/webpack.config.js
@@ -5,10 +5,11 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: 
-    path.resolve(__dirname,'./src/index.jsx'),
+    path.resolve(__dirname,'./src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "[name].boundle.js"
+    filename: "[name].boundle.js",
+    publicPath: '/'
   },
   module:{
     rules:[
@@ -23,25 +24,35 @@ module.exports = {
         }
       },
       {
-        test: /\.(css|less)$/,
-        use: ["style-loader", "css-loader"],
+          test: /\.(css|less)$/,
+          use: ["style-loader", "css-loader"],
+      },
+      // postcss 自动添加css的兼容前缀 即各个浏览器的兼容写法 -webcahi  之类
+      {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 
+          // {
+          //   loader: 'postcss-loader',  //单独对postloader进行相关配置 或者新建一个postcss.config.js来配置
+          //   options: {
+          //     plugins: [
+          //       require('autoprefixer')
+          //     ]
+          //   }
+          // }, 
+          'sass-loader']
       },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-       },
-       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              // outputPath: "images/",
-              esModule: false
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                // outputPath: "images/",
+                esModule: false
+              },
             },
-          },
-          ]
+            ]
         }
     ]  
     },
@@ -51,9 +62,10 @@ module.exports = {
     //   }),
     new HtmlWebpackPlugin({
       title: "首页",
-      // filename: "main.html",   //,默认为index.html
-      template: './src/wiew.html'  //指定写的html 模板路径
-    })
+      template: './src/main.html'  //指定自定义写的html 模板路径
+      // filename: "index.html",   //,生成预览页名称，默认为index.html 在内存里 不在目录
+    }),
+    
   ],
   devServer: {
     contentBase: './dist', // 告诉服务器从哪里提供内容
@@ -61,6 +73,6 @@ module.exports = {
     open: true, 
     hot: true, // 启用 webpack 的模块热替换特性
     // hotOnly: true // 当编译失败之后不进行热更新
-},
-
+    historyApiFallback: true // 路由刷新跳转404
+  },
 }
