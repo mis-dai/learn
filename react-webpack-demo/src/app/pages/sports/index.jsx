0b9b8c8c77e087277e53cb05c22a4dbd12586fba
mis-dai <67568929+mis-dai@users.noreply.github.com>
2021-12-07 11:36:56 +0800

    Create index.jsx

--- react-webpack-demo/src/app/pages/sports/index.jsx
+++ react-webpack-demo/src/app/pages/sports/index.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {store, Context} from './store.js'
 import { Detail } from './detail.jsx'
-
+import { post, get } from '../../../axios.js'
 
 export default () => {
   const values = store()
   const {data, setData} = values
+    const root = useRef(null)
+  console.log(this)
+  useEffect(() => {
+    request()
+    console.log('effect',Detail,this)
+    console.log('root',root)
+  },[])
+
+  // 请求
+  const request = ()  => {
+    post('https://preapi.qilelive.com/web/activity/ranking/anchor/stage',
+    {
+      stageId: 247,
+      pageSize: 30
+    }
+    ).then(res => {
+      console.log(res)
+    })
+  }
+  
   return(
     <Context.Provider value={values}>
         <h1>测试是否接收store的数据:{data.age}</h1>
         <Detail />
+        <div ref={root}>i hate the work</div>
     </Context.Provider>
   )
 }
